c83d241fc98a42f378e853b1370c26f7842fc046
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-14 07:02:13 -0400

    changes for assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.left = "100px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key ==="ArrowRight") {
+        moveDodgerRight();
+    }
+});
